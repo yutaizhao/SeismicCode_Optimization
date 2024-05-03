25494d2eb57329a3cb7626459cbb9ac990eb8032
“Yutai <yutai.zhao@gmail.com>
2024-05-03 21:11:45 +0200

    opt versions

--- 2/build/CMakeFiles/top-stencil.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for top-stencil.
-# This may be replaced when dependencies are built.
---
+++ pow direct/build/src/CMakeFiles/stencil.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/scratch/students/users/user1134/TOP/TOP-project/3/src/stencil/comm_handler.c" "src/CMakeFiles/stencil.dir/stencil/comm_handler.c.o" "gcc" "src/CMakeFiles/stencil.dir/stencil/comm_handler.c.o.d"
+  "/scratch/students/users/user1134/TOP/TOP-project/3/src/stencil/config.c" "src/CMakeFiles/stencil.dir/stencil/config.c.o" "gcc" "src/CMakeFiles/stencil.dir/stencil/config.c.o.d"
+  "/scratch/students/users/user1134/TOP/TOP-project/3/src/stencil/init.c" "src/CMakeFiles/stencil.dir/stencil/init.c.o" "gcc" "src/CMakeFiles/stencil.dir/stencil/init.c.o.d"
+  "/scratch/students/users/user1134/TOP/TOP-project/3/src/stencil/mesh.c" "src/CMakeFiles/stencil.dir/stencil/mesh.c.o" "gcc" "src/CMakeFiles/stencil.dir/stencil/mesh.c.o.d"
+  "/scratch/students/users/user1134/TOP/TOP-project/3/src/stencil/solve.c" "src/CMakeFiles/stencil.dir/stencil/solve.c.o" "gcc" "src/CMakeFiles/stencil.dir/stencil/solve.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
