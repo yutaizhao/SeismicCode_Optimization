70f3648ad7c7a6d6c865e12f3482820a9808649b
“Yutai <yutai.zhao@gmail.com>
2024-05-05 06:01:14 +0200

    multiprocessus opt

---
+++ other_versions/8)best + MPI re-size/build/src/CMakeFiles/stencil.dir/stencil/init.c.o.d
@@ -0,0 +1,28 @@
+src/CMakeFiles/stencil.dir/stencil/init.c.o: \
+ /scratch/students/users/user1134/TOP-project/best/src/stencil/init.c \
+ /usr/include/stdc-predef.h \
+ /scratch/students/users/user1134/TOP-project/best/src/../include/stencil/init.h \
+ /scratch/students/users/user1134/TOP-project/best/src/../include/stencil/mesh.h \
+ /scratch/students/users/user1134/TOP-project/best/src/../include/stencil/../types.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1/include/stdbool.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1/include/stddef.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1/include/stdint.h \
+ /usr/include/stdint.h /usr/include/bits/libc-header-start.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/bits/wordsize.h /usr/include/bits/timesize.h \
+ /usr/include/sys/cdefs.h /usr/include/bits/long-double.h \
+ /usr/include/gnu/stubs.h /usr/include/gnu/stubs-64.h \
+ /usr/include/bits/types.h /usr/include/bits/typesizes.h \
+ /usr/include/bits/time64.h /usr/include/bits/wchar.h \
+ /usr/include/bits/stdint-intn.h /usr/include/bits/stdint-uintn.h \
+ /usr/include/bits/stdint-least.h \
+ /scratch/students/users/user1134/TOP-project/best/src/../include/stencil/comm_handler.h \
+ /usr/include/mpi.h /usr/include/mpi_portable_platform.h \
+ /usr/lib/gcc/x86_64-pc-linux-gnu/13.2.1/include/omp.h \
+ /usr/include/assert.h /usr/include/math.h \
+ /usr/include/bits/math-vector.h /usr/include/bits/libm-simd-decl-stubs.h \
+ /usr/include/bits/floatn.h /usr/include/bits/floatn-common.h \
+ /usr/include/bits/flt-eval-method.h /usr/include/bits/fp-logb.h \
+ /usr/include/bits/fp-fast.h \
+ /usr/include/bits/mathcalls-helper-functions.h \
+ /usr/include/bits/mathcalls.h
