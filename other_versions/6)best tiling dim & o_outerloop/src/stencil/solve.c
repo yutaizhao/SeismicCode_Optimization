6f185ebd205c2a903cd705a0b1c80a320c9ecb7f
“Yutai <yutai.zhao@gmail.com>
2024-05-05 19:30:37 +0200

    versions 6 adjusted to get data

--- other_versions/6)best tiling dim & o_outerloop/src/stencil/solve.c
+++ other_versions/6)best tiling dim & o_outerloop/src/stencil/solve.c
@@ -13,9 +13,9 @@ void solve_jacobi(mesh_t* A, mesh_t const* B, mesh_t* C) {
     usz const dim_x = A->dim_x;
     usz const dim_y = A->dim_y;
     usz const dim_z = A->dim_z;
-    usz bloci = 8;
-    usz blocj = 8;
-    usz block = 8;
+    usz bloci = 10 ;
+    usz blocj = 40;
+    usz block = 40;
     
     for (usz o = 1; o <= STENCIL_ORDER; ++o) {
         f64 denom = 1/pow(17.0,(f64)o);
